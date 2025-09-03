95df03cb8612e3fe25f3111fc943882ab96b88e6
Kise1205 <unokiss307@gmail.com>
2025-09-03 10:48:37 -0500

    09/03/2025

--- hello-world-main/scripts/scripts.js
+++ hello-world-main/scripts/scripts.js
@@ -1,1 +1,13 @@
-console.log("we workin'!");
+
+document.
+    addEventListener()("DOMContentLoader",()=>{
+    console.log("we workin'!");
+    document.getElementById('btnClicky').addEventListener('click',btnClicky);
+    }
+);
+function btnClicky(){
+    console.log("in btnClicky event");
+    document.getElementById("btnClicky").style.transform = 'rotate(15deg)'
+    console.log("After rotation");
+}
+// const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { // ...now do DOM stuff });
