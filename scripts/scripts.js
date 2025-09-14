6fec584195a764501f9385b2cf155f433658460b
Kise1205 <unokiss307@gmail.com>
2025-09-14 15:00:36 -0500

    Add files via upload
    09/14/2025

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,13 +2,14 @@
 document.
     addEventListener("DOMContentLoaded",()=>{
     console.log("we workin'!");
-    // document.getElementById('btnClicky').addEventListener('click',btnClicky);
+    document.getElementById('btnClicky').addEventListener('click',btnClicky);
     }
 );
 function btnClicky(){
     console.log("in btnClicky event");
     // document.getElementById("btnClicky").style.transform = 'rotate(15deg)'
-    console.log("After rotation");
+    alert("Hello! Message has been sent 😊");
+    console.log("Message sent!!!");
 }
 // const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { // ...now do DOM stuff });
 
