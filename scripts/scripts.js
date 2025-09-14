8a6ebbbce1982b0ced25e236c68454cc2287f5d4
Thuan Nguyen <tnguyen20@una.edu>
2025-09-13 21:07:09 -0500

    09/13/25 9pm

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,12 +2,31 @@
 document.
     addEventListener("DOMContentLoaded",()=>{
     console.log("we workin'!");
-    document.getElementById('btnClicky').addEventListener('click',btnClicky);
+    // document.getElementById('btnClicky').addEventListener('click',btnClicky);
     }
 );
 function btnClicky(){
     console.log("in btnClicky event");
-    document.getElementById("btnClicky").style.transform = 'rotate(15deg)'
+    // document.getElementById("btnClicky").style.transform = 'rotate(15deg)'
     console.log("After rotation");
 }
 // const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { // ...now do DOM stuff });
+
+var currentDog = 1;
+// document.getElementById("imageContainer").onclick = function() {
+$("#imageContainer").on('click', function() {
+    if (currentDog == 1)
+    {
+        document.getElementById("doge-1").style.display = 'none';
+        document.getElementById("doge-2").style.display = 'block';
+        // $("#doge-1").hide();
+        // $("#doge-2").show();
+        currentDog = 2;
+    }
+    else
+    {
+        $("#doge-2").hide();
+        $("#doge-1").show();
+        currentDog = 1;
+    }
+});
