e8e15dd9306563dc0c547d56119379768bccf838
Kise1205 <unokiss307@gmail.com>
2025-09-15 09:00:07 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,21 +13,19 @@ function btnClicky(){
 }
 // const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { // ...now do DOM stuff });
 
-var currentDog = 1;
-// document.getElementById("imageContainer").onclick = function() {
-$("#imageContainer").on('click', function() {
-    if (currentDog == 1)
+var currentImg = 1;
+document.getElementById("imageContainer").onclick = function() {
+
+    if (currentImg == 1)
     {
         document.getElementById("doge-1").style.display = 'none';
         document.getElementById("doge-2").style.display = 'block';
-        // $("#doge-1").hide();
-        // $("#doge-2").show();
-        currentDog = 2;
+        currentImg = 2;
     }
     else
     {
-        $("#doge-2").hide();
-        $("#doge-1").show();
-        currentDog = 1;
+        document.getElementById("doge-1").style.display = 'block';
+        document.getElementById("doge-2").style.display = 'none';
+        currentImg = 1;
     }
-});
+};
