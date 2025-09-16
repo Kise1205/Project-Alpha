440084f3dc6d5b720db47feb6207721046920d45
Kise1205 <unokiss307@gmail.com>
2025-09-16 15:50:55 -0500

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,15 +2,15 @@
 document.
     addEventListener("DOMContentLoaded",()=>{
     console.log("we workin'!");
-    document.getElementById('btnClicky').addEventListener('click',btnClicky);
+    // document.getElementById('btnClicky').addEventListener('click',btnClicky);
     }
 );
-function btnClicky(){
-    console.log("in btnClicky event");
-    // document.getElementById("btnClicky").style.transform = 'rotate(15deg)'
-    alert("Hello! Message has been sent 😊");
-    console.log("Message sent!!!");
-}
+// function btnClicky(){
+//     console.log("in btnClicky event");
+//     // document.getElementById("btnClicky").style.transform = 'rotate(15deg)'
+//     alert("Hello! Message has been sent 😊");
+//     console.log("Message sent!!!");
+// }
 // const btnClicky = document.getElementById('btnClicky'); btnClicky.addEventListener('click', () => { // ...now do DOM stuff });
 
 var currentImg = 1;
